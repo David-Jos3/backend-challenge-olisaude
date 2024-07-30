13ea829dfdd12203123ae9610d3a58c9ad72fcee
David-jos3 <davidgomesgc230@gmail.com>
2024-07-29 21:12:45 -0300

    feat: setup Fastify server with client and health problem routes

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import fastify from 'fastify'
 import { clientsRouters } from './http/modules/client/router/router'
 import { healthProblemRoutes } from './http/modules/health-problem/router/router'
@@ -7,8 +6,3 @@ export const app = fastify()
 
 app.register(clientsRouters)
 app.register(healthProblemRoutes)
-=======
-import fastify from "fastify";
-
-export const app = fastify();
->>>>>>> 76a7da8aac4d275ae09809729353f1fca9e9fef2
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { env } from './env/index'
 import { app } from './app'
 
@@ -8,12 +7,3 @@ app
     port: env.PORT,
   })
   .then(() => console.log(`🥶 HTTP Server Running on PORT:${env.PORT} `))
-=======
-import {env} from './env/index'
-import {app} from './app'
-
-app.listen({
-  host: '0.0.0.0',  // Change to 'localhost' if running locally
-  port: env.PORT
-}).then(() => console.log(`🥶 HTTP Server Running on PORT:${env.PORT} `))
->>>>>>> 76a7da8aac4d275ae09809729353f1fca9e9fef2
